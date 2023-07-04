138a0fffc855b78a278eec45a4adcc041e0cceca
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2023-07-04 13:45:59 -0300

    add male and female symbols to h1 titles and general texts

--- src/app/page.js
+++ src/app/page.js
@@ -43,12 +43,12 @@ export default function Home() {
         <p className="text-center">
           Pantalones de calidad, directo desde la fábrica hasta su domicilio
         </p>
-        <Redes/>
+        <Redes />
       </section>
 
       <section name="TIRA BLACK" className="w-full bg-gray-800 p-6 text-center">
         <div className="text-white text-center p-8 text-xl">
-          ¡Mirá el catálogo, hacé tu pedido, y esperá la entrega!
+          <p>Mirá nuestro catálogo...</p> <p>¡no vas a creer nuestros precios!</p>
         </div>
         <span className="p-2 border-2 border-sky-700 rounded-lg hover:bg-sky-900">
           Compra mínima: 20 unidades
@@ -56,7 +56,8 @@ export default function Home() {
       </section>
 
       <section className="p-4 md:p-24">
-        <h1 id="damas" className="text-3xl text-center px-6 mt-12 font-black">
+        <AiOutlineWoman id="damas" className="mx-auto text-6xl text-pink-300 hover:scale-125 hover:text-pink-500" />
+        <h1  className="text-3xl text-center px-6 font-black">
           DAMAS
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 m-8 md:m-12 mt-12 text-center">
@@ -71,7 +72,10 @@ export default function Home() {
             />
           ))}
         </div>
-        <h1 id="hombre" className="text-3xl text-center px-6 mt-32 font-black">HOMBRE</h1>
+        <AiOutlineMan id="hombre" className="mx-auto text-6xl text-sky-300 hover:scale-125 hover:text-sky-500" />
+        <h1 className="text-3xl text-center px-6 font-black">
+          HOMBRES
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 m-8 md:m-12 mt-12 text-center">
           {Jeans.hombre.map((hombre) => (
             <GridComponent
